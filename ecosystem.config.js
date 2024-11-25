bd78e870118b4d44406d267b629aa088655b6763
sergeypeterman <mail@sergeypeterman.com>
2024-11-25 08:25:56 +0000

    scratch and temp-images added to .gitignore

---
+++ ecosystem.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  script: "jisao-bot.js",
+  // Specify which folder to watch
+  watch: ["src"],
+  // Specify delay between watch interval
+  watch_delay: 1000,
+  // Specify which folder to ignore
+  ignore_watch: ["node_modules", "scratch", "temp-images"],
+};
